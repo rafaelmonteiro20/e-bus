e244d0d6b9581148612408cef62ad47b11d8ab31
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-05-07 22:15:34 -0300

    Primeiro commit

---
+++ src/main/java/com/ebus/EBusApplication.java
@@ -0,0 +1,13 @@
+package com.ebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBusApplication.class, args);
+	}
+	
+}
